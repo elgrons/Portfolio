47a14436d71c82cd6262919c19bb7bcd92d04e96
Eliot Gronstal <eliot.lauren@gmail.com>
2023-05-29 18:34:57 -0700

    Emailjs messaging is functional.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../assets/img/floral.png";
 import emailjs from '@emailjs/browser';
@@ -16,6 +16,8 @@ export const Contact = () => {
   const [buttonText, setButtonText] = useState('Send');
   const [status, setStatus] = useState({});
 
+  const contactForm = useRef();
+
   const onFormUpdate = (category, value) => {
       setFormDetails({
         ...formDetails,
@@ -26,13 +28,13 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_hn2k3l3', 'template_4a4qvhk', form.current, 'mWle9qoDvq1M4MdfL')
+    emailjs.sendForm('service_69kndfc', 'template_7d4o7hb', contactForm.current, 'stlnMh3EBhbxeDTsl')
       .then((result) => {
           console.log(result.text);
-          console.log("Email sent successfully!");
+          console.log("Sent successfully!");
       }, (error) => {
           console.log(error.text);
-          console.log("Email failed to send!");
+          console.log("Failure to send.");
       });
   };
 
@@ -71,19 +73,19 @@ export const Contact = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <h2>Connect with Me</h2>
-                <form onSubmit={handleSubmit}>
+                <form ref={contactForm} onSubmit={handleSubmit}>
                   <Row>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                      <input type="text" value={formDetails.firstName} name="firstName" placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                      <input type="text" value={formDetails.lastName} name="lastName" placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="email" value={formDetails.email} placeholder="Contact Email" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                      <input type="email" value={formDetails.email} name="email" placeholder="Contact Email" onChange={(e) => onFormUpdate('email', e.target.value)} />
                     </Col>
                     <Col size={12} className="px-1">
-                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                      <textarea rows="6" value={formDetails.message} name="message" placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                       <button type="submit"><span>{buttonText}</span></button>
                     </Col>
                     {
