baaffa24fd542f3e29ca00ccc7d87ac227b00adb
Eliot Gronstal <eliot.lauren@gmail.com>
2023-05-31 16:46:55 -0700

    update font sizes on Project panes.

--- src/components/ProjectFolio.js
+++ src/components/ProjectFolio.js
@@ -5,7 +5,7 @@ export const ProjectFolio = ({ title, description, details, imgUrl }) => {
     <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <div className="proj-txtx">
-          <h4>{title}</h4>
+          <h5>{title}</h5>
           <span>{description}</span><br></br>
           <p>{details}</p>
         </div>
