cf3d991801d79dca00c710a3a84ca6b557e03f82
Eliot Gronstal <eliot.lauren@gmail.com>
2023-08-22 12:04:14 -0700

    update fonts and resume link.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -15,16 +15,16 @@ export const Intro = () => {
             <p>In my free time I enjoy:</p>
             <h3><Flower1 size={32} /> Practicing vinyasa yoga</h3><br></br>
             <h3><Flower1 size={32} /> Listening to music and attending concerts</h3><br></br>
-            <h3><Flower1 size={32} /> Collecting recipes and cooking plant-based goodies</h3><br></br>
-            <h3><Flower1 size={32} /> Devouring good stories: <a href=
-            "https://www.goodreads.com/user/show/7976181-eliot">science fiction & fantasy novels </a> especially, but I love TV shows and movies, too! </h3><br></br>
-            <h3><Flower1 size={32} /> My two cats: Handsome & Holiday</h3><br></br>
+            <h3><Flower1 size={32} /> Collecting and cooking plant-based recipes</h3><br></br>
+            <h3><Flower1 size={32} /> Devouring good stories (<a href=
+            "https://www.goodreads.com/user/show/7976181-eliot">science fiction & fantasy novels </a> especially, but TV shows and movies, too!) </h3><br></br>
+            <h3><Flower1 size={32} /> My cats: Handsome & Holiday</h3><br></br>
           </div>
             </Col>
             <Col xs={12} s={8} md={7}>
             <div>
                 <span className="tagline">About Me</span>
-                  <p>I'm an passionate communicator and I love to explore the fascinating ways people connect, both online and offline.<br></br><br></br> After 8.5 years of working for a <a href="https://breakside.com/">world-class microbrewery</a>, I decided to pivot and pursue a career in software development. I completed a certificate in Mobile and Web Development at <a href="https://www.epicodus.com/">Epicodus</a> in 2023.</p>
+                  <p>I'm an passionate communicator and I love to explore the fascinating ways people connect, both online and offline.<br></br><br></br> After 8.5 years of working for a <a href="https://breakside.com/">world-class microbrewery</a>, I decided to pivot and pursue a career in software development. I completed a certificate in Mobile and Web Development at Epicodus in 2023.</p>
                   <br></br>
                   <p>I bring my top-notch communication skills, organizational prowess, and growth mindset to every team and project I'm part of.</p>
                   <br></br>
