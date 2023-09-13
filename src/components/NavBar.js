077b1f5785047e75fd42b17343f67b9a2e17f6e3
Eliot Gronstal <eliot.lauren@gmail.com>
2023-09-13 10:33:29 -0700

    update resume link in navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -46,7 +46,7 @@ export const NavBar = () => {
                 <a href="https://www.linkedin.com/in/elgrons/"><img src={navIcon1} alt="LinkedIn nav icon" /></a>
                 <a href="https://github.com/elgrons"><img src={navIcon2} alt="GitHub nav icon" /></a>
               </div>
-              <a href="https://drive.google.com/file/d/1fNnACfZnr8Cx1UYO3Yres0uia4GDkbzZ/view?usp=sharing">
+              <a href="https://drive.google.com/file/d/1KoEN7B3W3AVOVbh1LCOKOZ0JW04SXjcP/view?usp=sharing">
               <button className="vvd" onClick={() => console.log ('resume')}><span>Review My Resume</span></button></a>
             </span>
           </Navbar.Collapse>
