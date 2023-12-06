1c01e29af690d046d52cf106a4e094f780b1ed9f
Eliot Gronstal <eliot.lauren@gmail.com>
2023-12-06 11:13:36 -0800

    update wording in Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -55,8 +55,8 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome!</span>
-                <h1>{`Hi! I'm Eliot:`}<br></br> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Full Stack Developer", "Solutions Engineer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>I love opportunities to merge my ability to connect with others and passion for developing innovative solutions.<br></br></p>
+                <h1>{`Hi! I'm Eliot:`}<br></br> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Full Stack Developer", "Software Engineer" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Collaborative and empathetic full stack web developer with a penchant for optimizing brand narratives and delivering innovative solutions.<br></br></p>
                   <a href="mailto: eliot.lauren@gmail.com">
                   <button onClick={() => console.log('contact')}>Let’s connect! <Envelope size={38} /></button></a>
               </div>}
