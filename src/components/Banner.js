0f5c63f94d81898e0ed3c6e57a47b6ee6f670c36
Eliot Gronstal <eliot.lauren@gmail.com>
2025-11-30 11:43:56 -0800

    update tagline in Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const toRotate = [ "Full Stack Developer", "Software Engineer" ];
+  const toRotate = [ "Full Stack Web Dev", "Client Success" ];
   const period = 2000;
 
   useEffect(() => {
@@ -56,7 +56,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome!</span>
                 <h1>{`Hi! I'm Eliot:`}<br></br> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Full Stack Developer", "Software Engineer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Collaborative and empathetic full stack web developer with a penchant for optimizing brand narratives and delivering innovative solutions.<br></br></p>
+                  <p>Collaborative and empathetic full stack web developer with a customer-focused mindset looking to optimize product experieneces and deliver innovative solutions.<br></br></p>
                   <a href="mailto: eliot.lauren@gmail.com">
                   <button onClick={() => console.log('contact')}>Let’s connect! <Envelope size={38} /></button></a>
               </div>}
